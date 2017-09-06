c0bc8ea27b0c2dc2e4e5a32d63878de658234fdb
cjmodis <cjmodis@gmail.com>
2017-09-06 21:09:44 +0300

    change Interface

--- mpPrPosition_2014/Interface.cs
+++ mpPrPosition_2014/Interface.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using ModPlusAPI.Interfaces;
 
 namespace mpPrPosition
@@ -8,7 +9,9 @@ namespace mpPrPosition
         public SupportedProduct SupportedProduct => SupportedProduct.AutoCAD;
         public string Name => "mpPrPosition";
         public string AvailProductExternalVersion => "2014";
-        public string ClassName => string.Empty;
+        public string FullClassName => string.Empty;
+        public string AppFullClassName => string.Empty;
+        public Guid AddInId => Guid.Empty;
         public string LName => "Позиция изделий";
         public string Description => "Функция добавляет/удаляет позицию в выбранные изделия и позволяет проставить маркировку позиции";
         public string Author => "Пекшев Александр aka Modis";
